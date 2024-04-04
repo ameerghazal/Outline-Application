0d7b9c643006c50a1ab6de82dbd414e613ce6126
NirwaaanAz <nirwaan.azhar-1@ou.edu>
2024-04-04 15:57:21 -0500

    added a components and pages folders. There is a button component and an editable list. Also added PostPage.js and a Bullet.png bullet point

---
+++ components/OutlineEditList.js
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import { View, StyleSheet,Text,TextInput,TouchableOpacity, Image} from "react-native";
+
+const MaxItems = 5;
+TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
+
+const OutlineEditList = () => {
+  const [items, setItems] = useState(['Item 1']);
+
+  const addItem = (text) => {
+    if (items.length < MaxItems) {
+      setItems([...items, text]);
+    }
+  };
+
+  const updateItem = (index, newText) => {
+    const updatedItems = [...items];
+    updatedItems[index] = newText;
+    setItems(updatedItems);
+  };
+
+  const deleteItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  const handleKeyPress = (index, event) => {
+    if (event.nativeEvent.key === 'Backspace' && items[index] === '' && items.length > 1) {
+        deleteItem(index);
+    }
+    if (event.nativeEvent.key === 'Enter' && items.length < MaxItems) {
+        addItem('New Item');
+    }
+  };
+
+  const handleAddButtonClick = () => {
+    addItem('New Item');
+  };
+
+
+  return (
+    <View style={styles.listContainer}>
+      {items.map((item, index) => (
+        <View style={styles.itemContainer} key={index}>
+          <Image 
+          source={require('../assets/Bullet.png')} 
+          style={styles.bullet}
+          />
+          <TextInput
+            style={styles.input}
+            value={item}
+            multiline={false}
+            returnKeyType='next'
+            onChangeText={(text) => updateItem(index, text)}
+            onKeyPress={(event) => handleKeyPress(index, event)}
+          />
+        </View>
+      ))}
+      {items.length < MaxItems && (
+        <View style={styles.addItemContainer}>
+          <TouchableOpacity onPress={handleAddButtonClick} style={styles.addButton}>
+          <Text style={styles.newItemButton}>+</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    listContainer: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: '#1B1B1B',
+      alignItems: 'left',
+    },
+    listTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#FFFAFA',
+      marginBottom: 10,
+    },
+    itemContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 10,
+    },
+    newItemButton: {
+      fontSize: 30,
+      marginLeft: 2,
+      color: '#FFFAFA',
+    },
+    addButtonText: {
+      color: '#FFFAFA',
+    },
+    input: {
+      flex: 1,
+      padding: 10,
+      fontSize: 16,
+      color: '#FFFAFA',
+    },
+    bullet: {
+      width: 25,
+      height: 25,
+      resizeMode: 'stretch',
+    }
+  });
+
+  export default OutlineEditList;
---
+++ pages/PostPage.js
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, StyleSheet,Text,KeyboardAvoidingView} from "react-native";
+import Constants from 'expo-constants';
+import AppButton from '../components/AppButton'
+import OutlineEditList from './components/OutlineEditList'
+
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <AppButton title="Cancel" size="sm" color="#FFFAFA" backgroundColor="transparent"></AppButton>
+        <Text style={styles.header_text}>NEW POST</Text>
+        <AppButton title="Post" size="sm" backgroundColor="#8DAC83" color="FFFAFA"></AppButton>
+      </View>
+      <KeyboardAvoidingView style={styles.post}>
+        <OutlineEditList />
+      </KeyboardAvoidingView>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1B1B1B',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  header : {
+    width: '100%',
+    backgroundColor: '#1B1B1B',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginLeft: -10,
+    marginTop: Constants.statusBarHeight,
+    justifyContent: 'space-between',
+    padding: 15,
+    borderBottomWidth: .25,
+    borderBottomColor: '#656060',
+    borderBottomEndRadius: 1,
+  },
+  header_text: {
+    fontSize: 18,
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    color: '#8DAC83',
+  },
+  post: {
+    alignItems: 'left',
+  }
+});
