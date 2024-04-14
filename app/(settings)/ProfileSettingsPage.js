eac85c313a727b5bce420bff63bf1a8d0da76623
alinawaz04 <alihnawaz@gmail.com>
2024-04-14 14:35:21 -0500

    get state from profile settings input boxes

--- app/(settings)/ProfileSettingsPage.js
+++ app/(settings)/ProfileSettingsPage.js
@@ -2,15 +2,33 @@ import { View, Text } from "react-native";
 import { BackBar } from "../(user_auth)/Components";
 import globalStyles from "./globalStyles";
 import SettingsInputBox from "../components/SettingsInputBox";
+import { useState } from "react";
 
 function ProfileSettingsPage() {
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [campus, setCampus] = useState("");
+
   return (
     <View style={globalStyles.container}>
       <BackBar></BackBar>
-      <SettingsInputBox placeholder="Phone"></SettingsInputBox>
-      <SettingsInputBox placeholder="Email Address"></SettingsInputBox>
-      <SettingsInputBox placeholder="Address"></SettingsInputBox>
-      <SettingsInputBox placeholder="Campus"></SettingsInputBox>
+      <SettingsInputBox
+        onChangeText={setPhone}
+        placeholder="Phone"
+      ></SettingsInputBox>
+      <SettingsInputBox
+        onChangeText={setEmail}
+        placeholder="Email Address"
+      ></SettingsInputBox>
+      <SettingsInputBox
+        onChangeText={setAddress}
+        placeholder="Address"
+      ></SettingsInputBox>
+      <SettingsInputBox
+        onChangeText={setCampus}
+        placeholder="Campus"
+      ></SettingsInputBox>
     </View>
   );
 }
