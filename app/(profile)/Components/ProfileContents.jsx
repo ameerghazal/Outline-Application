d71d94bacaed6fc3fdcc2c647df985326bb88012
IbrahimUFlip <ibrahimmohammad@uflip.com>
2024-04-16 15:16:48 -0500

    added javadocs, settings navigation in header, tab navigation

--- app/(profile)/Components/ProfileContents.jsx
+++ app/(profile)/Components/ProfileContents.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 import { View, Text, Image, TouchableOpacity, Pressable } from "react-native";
-import { frames } from "../styles";
+import { profileContents } from "../styles";
 
+/**
+ * Component for the top page of the Profile Page.
+ * Functionality: Clicking pfp or edit button pops modal allowing you to edit your account information
+ * @author: Ibrahim Mohammad
+ * @returns Top of page containing pfp, name, stats(num posts/following), bio
+ */
 export const ProfileContents = ({ userData }) => {
   return (
-    <View style={frames.ProfileContents}>
+    <View style={profileContents.ProfileContents}>
       <ProfileImage imageURL={userData.imageURL}></ProfileImage>
       <ProfileName
         displayName={userData.displayName}
         displayHandle={userData.displayHandle}
       ></ProfileName>
 
-      <View style={frames.ProfileDetailContainer}>
+      <View style={profileContents.ProfileDetailContainer}>
         <ProfileStatsBar
           outlineCt={userData.outlineCt}
           followerCt={userData.followerCt}
@@ -25,36 +31,55 @@ export const ProfileContents = ({ userData }) => {
 
 const ProfileBio = ({ children }) => {
   return (
-    <View style={frames.ProfileBioContainer}>
-      <Text style={frames.BioText}>{children}</Text>
+    <View style={profileContents.ProfileBioContainer}>
+      <Text style={profileContents.BioText}>{children}</Text>
     </View>
   );
 };
 
 const ProfileStatsBar = ({ outlineCt, followerCt, followingCt }) => {
   return (
-    <View style={frames.ProfileStatsContainer}>
-      <Text style={frames.DisplayStats}>{outlineCt}k outlines</Text>
-      <Text style={frames.DisplayStats}>{followerCt} followers</Text>
-      <Text style={frames.DisplayStats}>{followingCt} following</Text>
-      <Pressable style={frames.BtnEditProfile}>Edit Profile</Pressable>
+    <View style={profileContents.ProfileStatsContainer}>
+      <ProfileStat label="outlines">
+        <Text>{`${outlineCt}k`}</Text>
+      </ProfileStat>
+      <ProfileStat label="followers">
+        <Text>{`${followerCt}k`}</Text>
+      </ProfileStat>
+      <ProfileStat label="following">
+        <Text>{`${followingCt}k`}</Text>
+      </ProfileStat>
+      <EditProfileBtn></EditProfileBtn>
     </View>
   );
 };
 
+const EditProfileBtn = () => {
+  return (
+    <Pressable style={profileContents.BtnEditProfile}>Edit Profile</Pressable>
+  );
+};
+
+const ProfileStat = ({ children, label }) => (
+  <Text style={profileContents.DisplayStats}>
+    {children}
+    <Text style={profileContents.StatTextStyle}> {label}</Text>
+  </Text>
+);
+
 const ProfileName = ({ displayName, displayHandle }) => {
   return (
-    <View style={frames.ProfileNameContainer}>
-      <Text style={frames.DisplayName}>{displayName}</Text>
-      <Text style={frames.DisplayHandle}>@{displayHandle}</Text>
+    <View style={profileContents.ProfileNameContainer}>
+      <Text style={profileContents.DisplayName}>{displayName}</Text>
+      <Text style={profileContents.DisplayHandle}>@{displayHandle}</Text>
     </View>
   );
 };
 
 export const ProfileImage = ({ imageURL }) => {
   return (
-    <TouchableOpacity style={frames.IconContainer}>
-      <Image source={imageURL} style={frames.ProfileIcon}></Image>
+    <TouchableOpacity style={profileContents.IconContainer}>
+      <Image source={imageURL} style={profileContents.ProfileIcon}></Image>
     </TouchableOpacity>
   );
 };
